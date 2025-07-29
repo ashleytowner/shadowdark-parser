bccbba810edc0d45f916fd0481d98a3f97486253
Ashley Towner <git@ashtowner.com>
2025-07-29 22:46:26 +0930

    feat: Created parser function

---
+++ parser.js
@@ -0,0 +1,184 @@
+/**
+ * @param {string} str
+ * @param {string} sub
+ */
+function splitBeforeSubstring(str, sub) {
+  const index = str.indexOf(sub);
+  if (index === -1) {
+    return ["", str];
+  } else {
+    return [str.slice(0, index), str.slice(index, str.length).replace(sub, "")];
+  }
+}
+
+/**
+ * @param {string} attack
+ */
+function parseAttack(attack) {
+  // const [_, quantity, name, bonus, __, ___, damage] = attack.match(
+  let matches = attack.match(
+    /^(?<qty>\d+) (?<name>.+)( (?<bonus>(\+|-)(\d+)) (?<damage>\(.+\)))$/,
+  );
+	if (!matches) {
+		matches = attack.match(/^(?<qty>\d+) (?<name>.*)$/)
+	}
+  return {
+		quantity: matches.groups.qty,
+    name: matches.groups.name,
+    bonus: matches.groups.bonus,
+    damage: matches.groups.damage,
+  };
+}
+
+/**
+ * @param {string} attacks
+ */
+function parseAttacks(attacks) {
+  let groups = attacks.split(" or ");
+  groups = groups.map((group) =>
+    group.split(" and ").map((atk) => parseAttack(atk)),
+  );
+  return groups;
+}
+
+/**
+ * @param {string} part
+ */
+function isAbilityStart(part) {
+  if (part.indexOf(".") === -1) return false;
+  if (part[0].toUpperCase() !== part[0]) return false;
+  const lastWordBeforePeriod = part.match(/\w+\./)[0];
+  if (lastWordBeforePeriod[0].toUpperCase() !== lastWordBeforePeriod[0]) {
+    return false;
+  }
+  return true;
+}
+
+/**
+ * @param {string[]} abilities
+ */
+function parseAbilities(abilities) {
+  const parsed = [];
+  let current = "";
+  for (let i = 0; i < abilities.length; i++) {
+    const line = abilities[i];
+    if (isAbilityStart(line)) {
+      parsed.push(current.trim());
+      current = line;
+    } else {
+      current += ` ${line}`;
+    }
+  }
+  parsed.push(current);
+  return parsed.filter(Boolean).map((ability) => {
+    const indexOfPeriod = ability.indexOf(". ");
+    const name = ability.slice(0, indexOfPeriod);
+    const description = ability.slice(indexOfPeriod + 1, ability.length);
+    return {
+      name,
+      description,
+    };
+  });
+}
+
+/**
+ * @param {string} statblockText
+ */
+function parseStatblock(statblockText) {
+  const lines = statblockText.split("\n").filter(Boolean);
+  const name = lines.shift();
+  let description = "";
+  while (!lines[0].startsWith("AC ")) {
+    description += ` ${lines.shift()}`;
+  }
+  description = description.trim();
+  let stats = "";
+  while (!/LV \d+$/.test(lines[0])) {
+    stats += ` ${lines.shift()}`;
+  }
+  stats += ` ${lines.shift()}`;
+  stats = stats.trim();
+
+  let result = splitBeforeSubstring(stats, ", ");
+  stats = result[1];
+  const ac = Number(result[0].split(" ")[1]);
+
+  const armor = result[0].split(" ")[2]?.replace(/(\(|\))/g, "") || "";
+
+  result = splitBeforeSubstring(stats, ", ATK ");
+  stats = result[1];
+  const hp = Number(result[0].split(" ")[1]);
+
+  result = splitBeforeSubstring(stats, ", MV");
+  stats = result[1];
+  const attacks = parseAttacks(result[0]);
+
+  result = splitBeforeSubstring(stats, ", S ");
+  stats = result[1];
+  const movement = result[0].trim();
+
+  result = splitBeforeSubstring(stats, ", D ");
+  stats = result[1];
+  const strength = Number(result[0]);
+
+  result = splitBeforeSubstring(stats, ", C ");
+  stats = result[1];
+  const dexterity = Number(result[0]);
+
+  result = splitBeforeSubstring(stats, ", I ");
+  stats = result[1];
+  const constitution = Number(result[0]);
+
+  result = splitBeforeSubstring(stats, ", W ");
+  stats = result[1];
+  const intelligence = Number(result[0]);
+
+  result = splitBeforeSubstring(stats, ", Ch ");
+  stats = result[1];
+  const wisdom = Number(result[0]);
+
+  result = splitBeforeSubstring(stats, ", AL ");
+  stats = result[1];
+  const charisma = Number(result[0]);
+
+  result = splitBeforeSubstring(stats, ", LV ");
+  stats = result[1];
+  const alignment = result[0].trim();
+
+  const level = Number(stats);
+
+  return {
+    name,
+    description,
+    ac,
+    armor,
+    hp,
+    attacks,
+    movement,
+    strength,
+    dexterity,
+    constitution,
+    intelligence,
+    wisdom,
+    charisma,
+    alignment,
+    level,
+    abilities: parseAbilities(lines),
+  };
+}
+
+// console.log(parseStatblock(`
+// CULTIST
+// A cloaked, wild-eyed zealot
+// chanting the guttural prayers of
+// a dark god.
+// AC 14 (chainmail + shield), HP
+// 9, ATK 1 longsword +1 (1d8) or 1
+// spell +2, MV near, S +1, D -1, C +0, I
+// -1, W +2, Ch +0, AL C, LV 2
+// Fearless. Immune to morale
+// checks.
+// Deathtouch (WIS Spell). DC 12.
+// 2d4 damage to one creature
+// within close.
+// `).attacks);
