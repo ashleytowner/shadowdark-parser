1779374f66c8059660360d6061f2020f4ad7ebc5
Ashley Towner <git@ashtowner.com>
2025-09-06 15:19:15 +0930

    feat(templates): Move template code from CLI to library

--- src/bin/shadowdark-parser.ts
+++ src/bin/shadowdark-parser.ts
@@ -3,33 +3,10 @@ import readline from "readline";
 import fs from "fs";
 import { program } from "commander";
 import { bulkParse, parse } from "../parser.js";
-import handlebars from "handlebars";
 import { allCapsStrategy } from "../splitbulk.js";
 import type { Entity } from "../entity.js";
-
-handlebars.registerHelper("eq", function (a, b) {
-  return a == b;
-});
-
-handlebars.registerHelper("signedNumber", function (value) {
-  if (typeof value === "string") {
-    value = Number(value);
-  }
-
-  if (value < 0) {
-    return String(value);
-  } else {
-    return `+${value}`;
-  }
-});
-
-handlebars.registerHelper("firstChar", function (value) {
-  if (typeof value === "string" && value.length > 0) {
-    return value.charAt(0);
-  } else {
-    return "";
-  }
-});
+import handlebars from "../handlebars.js";
+import getTemplateFromFile from "../handlebars.js";
 
 program
   .name("shadowdark-parser")
@@ -93,12 +70,7 @@ const filename = program.args[0];
   }
 
   if (options.template) {
-    const templateSource = fs.readFileSync(options.template, "utf8");
-    const template = handlebars.compile(templateSource);
-
-    const rendered = template(data);
-
-    data = rendered;
+    data = getTemplateFromFile(options.template)(data);
   } else {
     data = JSON.stringify(data);
   }
---
+++ src/handlebars.ts
@@ -0,0 +1,32 @@
+import handlebars from "handlebars";
+import fs from "fs";
+
+handlebars.registerHelper("eq", function (a, b) {
+  return a == b;
+});
+
+handlebars.registerHelper("signedNumber", function (value) {
+  if (typeof value === "string") {
+    value = Number(value);
+  }
+
+  if (value < 0) {
+    return String(value);
+  } else {
+    return `+${value}`;
+  }
+});
+
+handlebars.registerHelper("firstChar", function (value) {
+  if (typeof value === "string" && value.length > 0) {
+    return value.charAt(0);
+  } else {
+    return "";
+  }
+});
+
+export default function getTemplateFromFile(filename: string) {
+  const templateSource = fs.readFileSync(filename, "utf8");
+  const template = handlebars.compile(templateSource);
+  return template;
+}
